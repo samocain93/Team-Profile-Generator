76e016cccad5e2670dad710dac9cef0fcc7a8637
Sam O'Cain <samocain@Sams-MBP.lan>
2023-01-07 21:58:14 -0600

    updated engineer-card.js

--- src/engineer-card.js
+++ src/engineer-card.js
@@ -1,14 +1,28 @@
 function engineerCard(engineer) {
     return `
-    <div class="card">
-    <h3>${engineer.name}</h3>
-<h4>Engineer</h4>
-<ul>
-    <li>Employee ID: ${engineer.id}</li>
-    <li>Email: ${engineer.email}</li>
-    <li>GitHub Username: ${engineer.github}</li>
-</ul>
-</div>
+    <div class="card col-3 m-3 mb-5 employee-card shadow-lg">
+    <div class="card-header employee-header bg-primary">
+      <h3 class="card-title p-1">Kevin Flanagan</h3>
+      <h5 class="card-subtitle">
+        <i class="fa-brands fa-github"></i> Engineer
+      </h5>
+    </div>
+    <div class="card-body bg-light">
+      <ul class="list-group list-group-flush bg-white">
+        <li class="list-group-item">Employee ID: 200</li>
+        <li class="list-group-item">
+          Email Address:
+          <a href="mailto:kevin@fakemail.com">kevin@fakemail.com</a>
+        </li>
+        <li class="list-group-item">
+          GitHub Username:
+          <a href="https://github.com/kevinaok25" target="_blank"
+            >kevinaok25</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
     `
 }
 
